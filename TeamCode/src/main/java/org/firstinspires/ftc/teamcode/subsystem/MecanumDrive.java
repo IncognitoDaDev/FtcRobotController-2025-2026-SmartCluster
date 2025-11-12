ce275a63f5df4e076f9c063ff232e23e49f383e7
cogdev <incognitodadev@gmail.com>
2025-11-12 16:53:05 +0200

    OracleFTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
@@ -6,10 +6,14 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.smartcluster.oracleftc.commands.Command;
 import com.smartcluster.oracleftc.commands.Command.CommandBuilder;
+import com.smartcluster.oracleftc.hardware.OracleLynxVoltageSensor;
 import com.smartcluster.oracleftc.utils.ProcessedGamepad;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 public class MecanumDrive {
     private DcMotor frontRight, frontLeft, backRight, backLeft;
+    public final OracleLynxVoltageSensor voltageSensor;
+    public double baseSpeed = 1.0;
 
     public MecanumDrive(HardwareMap hardwareMap)
     {
@@ -25,34 +29,66 @@ public class MecanumDrive {
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        voltageSensor = hardwareMap.getAll(OracleLynxVoltageSensor.class).iterator().next();
+        voltageSensor.setPolicy(OracleLynxVoltageSensor.OracleLynxVoltageSensorPolicy.CACHED);
+        voltageSensor.setVoltageCacheFreshness(300);
+
+        setDriveMode(DriveMode.NORMAL);
     }
 
     public Command drive(ProcessedGamepad gamepad)
     {
         return new CommandBuilder()
                 .update(()->{
-                    double y = -gamepad.left_stick.get().y; // Remember, Y stick is reversed!
-                    double x = gamepad.left_stick.get().x; // Counteract imperfect strafing
+                    ProcessedGamepad.Joystick.JoystickData leftStick = gamepad.left_stick.get();
+                    ProcessedGamepad.Joystick.JoystickData rightStick = gamepad.right_stick.get();
 
-                    if(gamepad.left_trigger.get() < 0.1)
-                    {
-                        y/=2;
-                        x/=2;
-                    }
+                    double boost = (gamepad.left_bumper.get() || gamepad.right_bumper.get() ? 1 : 0);
 
-                    double denominator = Math.max(Math.abs(y) + Math.abs(x), 1);
-                    double frontLeftPower = (y + x) / denominator;
-                    double backLeftPower = (y - x) / denominator;
-                    double frontRightPower = (y - x) / denominator;
-                    double backRightPower = (y + x) / denominator;
+                    double rx = rightStick.x * boost;
+                    double x = leftStick.x * 1.0 * boost;
+                    double y = -leftStick.y * boost;
 
-                    frontRight.setPower(frontRightPower);
-                    frontLeft.setPower(frontLeftPower);
-                    backLeft.setPower(backLeftPower);
-                    backRight.setPower(backRightPower);
+                    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+                    double frontLeftPower = (y + x + rx) / denominator * baseSpeed;
+                    double backLeftPower = (y - x + rx) / denominator * baseSpeed;
+                    double frontRightPower = (y - x - rx) / denominator * baseSpeed;
+                    double backRightPower = (y + x - rx) / denominator * baseSpeed;
+
+                    setMotorPowers(frontRightPower, backRightPower, frontLeftPower, backLeftPower);
                 })
                 .build();
     }
+
+    enum DriveMode
+    {
+        NORMAL,
+        PREFIRE,
+        PARKING
+    }
+
+    public void setDriveMode(DriveMode mode)
+    {
+        switch(mode)
+        {
+            case NORMAL: baseSpeed = 1.0; break;
+            case PREFIRE: baseSpeed = 0.7; break;
+            case PARKING: baseSpeed = 0.4; break;
+        }
+    }
+
+
+    public void setMotorPowers(double frontRightPower, double backRightPower, double frontLeftPower,
+                               double backLeftPower) {
+
+        double voltage = voltageSensor.getVoltage();
+        frontRight.setPower(frontRightPower*(12.0/voltage));
+        backRight.setPower(backRightPower*(12.0/voltage));
+        frontLeft.setPower(frontLeftPower*(12.0/voltage));
+        backLeft.setPower(backLeftPower*(12.0/voltage));
+    }
+
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.smartcluster.oracleftc.commands.Command;
+import com.smartcluster.oracleftc.commands.ParallelCommand;
+
 public class Robot {
+    private final OpMode opMode;
+
+    public Robot(OpMode mode)
+    {
+        this.opMode = mode;
+    }
+
+    public Command reset()
+    {
+        return new ParallelCommand(
+
+        );
+    }
+
+    public Command update()
+    {
+        return new ParallelCommand(
+
+        );
+    }
+
 }
