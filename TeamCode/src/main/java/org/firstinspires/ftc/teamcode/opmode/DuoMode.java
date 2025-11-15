b5dc8116172b199233f2c965a34f17be866cd75f
cogdev <incognitodadev@gmail.com>
2025-11-15 14:09:29 +0200

    Pre-Odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DuoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DuoMode.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.smartcluster.oracleftc.commands.Command;
 import com.smartcluster.oracleftc.commands.CommandScheduler;
-import com.smartcluster.oracleftc.fsm.FSM;
+import com.smartcluster.oracleftc.commands.ParallelCommand;
+import com.smartcluster.oracleftc.hardware.OracleLynxVoltageSensor;
 import com.smartcluster.oracleftc.utils.ProcessedGamepad;
+
+import org.firstinspires.ftc.teamcode.subsystem.Robot;
+
+import java.util.List;
 
 
 @Config
@@ -16,20 +23,49 @@ public class DuoMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException
     {
-        waitForStart();
         ProcessedGamepad driverGamepad = new ProcessedGamepad(gamepad1),
                         operatorGamepad = new ProcessedGamepad(gamepad2);
+        Robot robot = new Robot(this);
+        List<LynxModule> modules = hardwareMap.getAll(LynxModule.class);
+
+        Command.run(robot.reset());
+        waitForStart(); // STARTING POINT
+
+        List<LynxModule> lynxModules = hardwareMap.getAll(LynxModule.class);
+        for(LynxModule lynxModule: lynxModules)
+            lynxModule.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+
+        List<OracleLynxVoltageSensor> voltageSensors =hardwareMap.getAll(OracleLynxVoltageSensor.class);
+        for (OracleLynxVoltageSensor voltageSensor :
+                voltageSensors) {
+            voltageSensor.setPolicy(OracleLynxVoltageSensor.OracleLynxVoltageSensorPolicy.CACHED);
+            voltageSensor.setVoltageCacheFreshness(100);
+
+        }
+
+
+        scheduler.schedule(
+                new ParallelCommand(
+                        robot.mecanumDrive.drive(driverGamepad)
+                )
+        );
 
         while(opModeIsActive())
         {
-            // do stuff
-        }
+            for(LynxModule lynxModule: lynxModules)
+                if(lynxModule.getSerialNumber().isEmbedded())
+                {
+                    lynxModule.clearBulkCache();
+                    lynxModule.getBulkData();
 
-        while(opModeIsActive() && !isStopRequested())
-        {
+                }
+
+            telemetry.addData("state", 1);
+            telemetry.update();
+
+
             driverGamepad.process();
             operatorGamepad.process();
-
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Robot.java
@@ -1,15 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.smartcluster.oracleftc.commands.Command;
 import com.smartcluster.oracleftc.commands.ParallelCommand;
 
 public class Robot {
     private final OpMode opMode;
+    public final MecanumDrive mecanumDrive;
 
     public Robot(OpMode mode)
     {
         this.opMode = mode;
+        this.mecanumDrive = new MecanumDrive(mode.hardwareMap, new Pose2d(0,0,0));
     }
 
     public Command reset()
